083cb81d9bef531b427c56cec8ec35f5cdcc7956
okamanda <olivier.kamanda@pif.gov>
2016-03-25 09:56:47 -0400

    added outgoing webhook for slackbot to replace slash command

--- slack_ikebot.php
+++ slack_ikebot.php
@@ -1,38 +1,55 @@
 <?php
 #This script uses slackbot to execute simple API search/response calls to open data
+include_once("../code/tokens.php");
+
 global $command;
-$command = $_POST['command']; //read the command from slack $_GET for testing & $_POST for production
+$command = $_POST['text']; //read the message from slack 
 
 error_reporting(E_ALL ^ E_DEPRECATED); //ignore those silly error messages until code crashes.
 
-function send_slack($message)
+
+
+function post_slack_msg($message)
 {
- /* prepare the message that slackbot sends back to the slack user */
-    $payload = json_encode(array('channel'=>'#general','username'=>'ChiefIke','text'=>$message,'icon_emoji'=>':dog:')); //parameters for sending the message
+    global $bot_token; //private authorization token defined in another file
+    if (empty($message)) {
+        $message = "Ignore this debugging message."; //if for some reason, the message is null    
+    }
     
-    /*set up the curl to transfer the message with certain header options */
-    $ch=curl_init();
+    $request_url       = "https://slack.com/api/chat.postMessage";
+    $parameters        = array(
+        'username' => 'chiefike',
+        'as_user' => 'true',
+        'channel' => '#general',
+        'text' => $message,
+        'token' => $bot_token
+    );
+    $query             = http_build_query($parameters);
+    $final_request_url = $request_url . "?" . $query;
+    $response          = file_get_contents($final_request_url);
     
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array ('Content-Type:application/json'));
-    curl_setopt($ch, CURLOPT_POSTFILEDS,$payload);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,2); //max timeout in seconds to connect
-    curl_setopt($ch, CURLOPT_TIMEOUT,4); //max timeout for the whole response
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_URL,'https://hooks.slack.com/services/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'); //webhook integration URL
     
-    $result=curl_exec($ch);
-    curl_close($ch);
     return true;
-    
 }
 
-
-
-if (strpos ($command, 'greet')==true)
+function parse_response($response)
 {
-   $text = $_POST['text'];
+    
+    if (strpos($response, 'economy') == true) {
+        post_slack_msg("Here's a snapshot of the US economy:");
+        
+    } else {
+        post_slack_msg("I don't know how to respond to that yet. But you can help me learn by visiting: http://www.pif.gov");
+    }
+    
+    return true;
+}
+if (strpos($command, 'chiefike') == true) {
+    $text  = $_POST['text'];
     $token = $_POST['token']; //store the token from the slack message and maybe use it for validation later
-send_slack("what's good homie?")    
+    
+    parse_response($text);
+    
 }
 
 ?>
